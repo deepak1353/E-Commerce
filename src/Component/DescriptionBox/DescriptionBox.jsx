1e9b8502c88e18b86ae519b634d1142002207f54
Deepak Yadav <deepakyadav20c@gmail.com>
2024-01-03 14:41:02 +0530

    ECommerce Frontend

---
+++ src/Component/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>    
+<div className="descriptionbox-navigator">
+<div className="descriptionbox-nav-box">Description</div>
+<div className="descriptionbox-nav-box fade" >Reviews(122)</div>
+</div>
+<div className="descriptionbox-description">
+    <p>Welcome to SHOPPER , your one-stop destination for a seamless online shopping experience. At Shopper, we redefine the way you shop with a curated selection of top-quality products, a user-friendly interface, and a commitment to customer satisfaction.
+      Browse through our diverse range of products, carefully curated to cater to your every need. From fashion and electronics to home essentials and beyond, we offer an extensive catalog featuring the latest trends and must-have items.</p>
+<p>Our dedicated customer support team is ready to assist you with any inquiries or concerns. Whether you have questions about products, orders, or returns, we're here to provide timely and friendly assistance.</p>
+</div>
+    </div>
+    
+  )
+}
+
+export default DescriptionBox
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartItems from "../Component/CartItems/CartItems";
+
+const Cart = () => {
+return (
+<>
+  <CartItems/>
+</>
+)
+
+}
+ export default Cart
